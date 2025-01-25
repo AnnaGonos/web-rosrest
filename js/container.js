f7d539875a2f2fc27a82fe634ba2a18f2861b563
gonos <gonosanna@mail.ru>
2025-01-25 21:08:24 +0300

    fix: переделала стиль, переместила боковую колонку в мейн

--- js/container.js
+++ js/container.js
@@ -1,23 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const aside = document.querySelector('aside');
-    const main = document.querySelector('main');
-    const footer = document.querySelector('footer');
-
-    const container = document.createElement('div');
-    container.className = 'columns-container';
-
-    container.style.display = 'flex';
-
-    const leftColumn = document.createElement('div');
-    leftColumn.style.flex = '1';
-
-    leftColumn.appendChild(main);
-
-    container.appendChild(leftColumn);
-    container.appendChild(aside);
-
-    document.body.appendChild(container);
-    if (footer) {
-        document.body.appendChild(footer);
-    }
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     const aside = document.querySelector('aside');
+//     const main = document.querySelector('main');
+//     const footer = document.querySelector('footer');
+//
+//     const container = document.createElement('div');
+//     container.className = 'columns-container';
+//
+//     container.style.display = 'flex';
+//
+//     const leftColumn = document.createElement('div');
+//     leftColumn.style.flex = '1';
+//
+//     leftColumn.appendChild(main);
+//
+//     container.appendChild(leftColumn);
+//     container.appendChild(aside);
+//
+//     document.body.appendChild(container);
+//     if (footer) {
+//         document.body.appendChild(footer);
+//     }
+// });
